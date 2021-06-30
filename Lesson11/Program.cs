bc4d3f72527090be17675d4b0133cfc4f6d2c3bc
Abdul Hajiyev <abdulhajiyev@gmail.com>
2021-06-30 07:28:38 -0800

    Lesson11_1

--- Lesson11/Program.cs
+++ Lesson11/Program.cs
@@ -2,6 +2,13 @@
 
 namespace Lesson11
 {
+    class A : IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
